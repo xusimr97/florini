2c4e5ecd9ca3556f3dd3ff6574777516286f034b
Josep <xusimr97@gmail.com>
2021-09-27 21:15:36 +0200

    Fixed error handler

--- back/server/middleware/error-handler.js
+++ back/server/middleware/error-handler.js
@@ -1,28 +1,29 @@
+const i18n = require('i18n')
 
-module.exports = function() {
-    return function(err, req, res, next) {
-        const statusCode = err.statusCode;
+module.exports = function () {
+  return function (err, req, res, next) {
+    const statusCode = err.statusCode
 
-        switch(statusCode) {
-        case 422: // Validation error
-            if (err.details) {
-                let messages = err.details.messages;
-                for (let message in messages) {
-                    let texts = messages[message];
-                    for (let i = 0; i < texts.length; i++) {
-                        if (!texts[i]) continue;
-                        texts[i] = req.__(texts[i]);
-                    }
-                }
-                err.message = req.__('invalidData');
-                break;
+    switch (statusCode) {
+      case 422: // Validation error
+        if (err.details) {
+          let messages = err.details.messages
+          for (let message in messages) {
+            let texts = messages[message]
+            for (let i = 0; i < texts.length; i++) {
+              if (!texts[i]) continue
+              texts[i] = i18.__n(texts[i])
             }
-        default:
-            if (statusCode >= 400 && statusCode < 500) {
-                err.message = req.__(err.message);
-            }
+          }
+          err.message = i18.__('invalidData')
+          break
         }
+      default:
+        if (statusCode >= 400 && statusCode < 500) {
+          err.message = i18.__(err.message)
+        }
+    }
 
-        next(err);
-    };
-};
+    next(err)
+  }
+}
