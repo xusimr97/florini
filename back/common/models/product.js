3732262ca84ad51bb5d82514347cf185e85f6cfc
Josep <xusimr97@gmail.com>
2021-10-12 14:22:29 +0200

    Delete and edit translations done

--- back/common/models/product.js
+++ back/common/models/product.js
@@ -7,6 +7,7 @@ const moment = require('moment') // require
 const writeFile = util.promisify(fs.writeFile)
 
 module.exports = function (Product) {
+  // Create Product Version
   Product.createProductVersion = async function (req, res) {
     try {
       await app.dataSources.db.transaction(async models => {
@@ -42,6 +43,108 @@ module.exports = function (Product) {
           })
         }
 
+        // Create ProductVersionTag
+        const productVersionTags = body.tags
+        for (const productVersionTag of productVersionTags) {
+          await ProductVersionTag.create({
+            productVersionId: productVersion.id,
+            tagId: productVersionTag.tag,
+            value: productVersionTag.value
+          })
+        }
+
+        // Create Images
+        const images = req.files
+        const date = moment().format('YYYY-MM-DD')
+        for (const [index, image] of images.entries()) {
+          const dir =
+            __dirname +
+            `/../../client/products/${req.params.id}/${productVersion.id}`
+
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true })
+          }
+
+          const url =
+            `products/${req.params.id}/${productVersion.id}/${date}-` +
+            makeid(7)
+
+          await writeFile(__dirname + `/../../client/` + url, image.buffer)
+
+          // Save url in Database
+          Image.create({
+            productVersionId: productVersion.id,
+            order: index,
+            url
+          })
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Edit Product Version
+  Product.editProductVersion = async function (req, res) {
+    try {
+      await app.dataSources.db.transaction(async models => {
+        const {
+          Product,
+          ProductVersion,
+          ProductVersionTranslation,
+          ProductVersionTag,
+          Image
+        } = models
+        const body = customBodyParse(req.body)
+        console.log(body)
+        // return
+
+        // Update Product Version
+        const productVersion = await ProductVersion.findById(
+          req.params.versionId
+        )
+        await productVersion.patchAttributes({
+          productId: JSON.parse(req.params.id),
+          ean: body.ean,
+          state: body.state,
+          price: body.price,
+          stock: body.stock,
+          sku: body.sku,
+          externalId: body.externalId
+        })
+
+        // Update ProductVersion Translations
+        const productVersionTranslations = body.translations
+
+        // Remove Translations
+        let arrayids = []
+        productVersionTranslations.forEach(productVersionTranslation => {
+          if (productVersionTranslation.id) {
+            arrayids.push(productVersionTranslation.id)
+          }
+        })
+        let toRemoveProductVersionTranslations = await ProductVersionTranslation.find(
+          { where: { id: { nin: arrayids } } }
+        )
+        for (const toRemoveProductVersionTranslation of toRemoveProductVersionTranslations) {
+          await ProductVersionTranslation.deleteById(
+            toRemoveProductVersionTranslation.id
+          )
+        }
+
+        // Upsert Translations
+        for (const productVersionTranslation of productVersionTranslations) {
+          // if()
+          await ProductVersionTranslation.upsert({
+            id: productVersionTranslation.id,
+            productVersionId: productVersion.id,
+            locale: productVersionTranslation.locale,
+            title: productVersionTranslation.title,
+            text: productVersionTranslation.text,
+            shortText: productVersionTranslation.shortText
+          })
+        }
+        return
         // Create ProductVersionTag
         const productVersionTags = body.tags
         for (const productVersionTag of productVersionTags) {
