183c005c309853f4d9e7e63b7c9f0b436f20c631
Josep <xusimr97@gmail.com>
2021-10-14 13:05:17 +0200

    Fixed custom login

--- back/common/models/customer.js
+++ back/common/models/customer.js
@@ -1,39 +1,67 @@
-const app = require('../../server/server');
+const app = require('../../server/server')
 
-module.exports = function(Self) {
+module.exports = function (Self) {
+  Self.CustomLogin = async function (req, res) {
+    try {
+      await app.dataSources.db.transaction(async models => {
+        const { Customer } = models
+        console.log(req.body)
+        const token = await Customer.login({
+          email: req.body.email,
+          password: req.body.password
+        })
+
+        const customer = await Customer.findById(token.userId)
+        if (customer.state) {
+          res.status(200).send(token)
+        } else {
+          res.status(400).send({ error: { code: 'USER_DESACTIVATED' } })
+        }
+      })
+    } catch (error) {
+      console.log(error)
+      res
+        .status(400)
+        .send({ error: { message: error.message, code: error.code } })
+    }
+  }
+
   Self.validatesUniquenessOf('email', {
     message: 'notUniqueEmail'
-  });
+  })
 
-  Self.validatePassword = function(password) {
+  Self.validatePassword = function (password) {
     if (!password) {
-      const err = new Error('passwordEmpty');
-      err.statusCode = 422;
-      throw err;
+      const err = new Error('passwordEmpty')
+      err.statusCode = 422
+      throw err
     }
 
     const pattern = new RegExp('(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.{6,})')
     if (!pattern.test(password)) {
-      const err = new Error('passwordRequeriments');
-      err.statusCode = 422;
-      throw err;
+      const err = new Error('passwordRequeriments')
+      err.statusCode = 422
+      throw err
     }
-  };
+  }
 
-  Self.on('resetPasswordRequest', function(info) {
-    const from = app.dataSources.email.settings.transports[0].auth.user;
+  Self.on('resetPasswordRequest', function (info) {
+    const from = app.dataSources.email.settings.transports[0].auth.user
 
-    const href = `http://localhost:8080/Customer/set-password?access_token=${info.accessToken.id}`;
-    const html = `Click <a href="${href}"> here</a> to reset your password`;
+    const href = `http://localhost:8080/Customer/set-password?access_token=${info.accessToken.id}`
+    const html = `Click <a href="${href}"> here</a> to reset your password`
 
-    Self.app.models.Email.send({
-      to: info.email,
-      from,
-      subject: 'Reset Password',
-      html
-    }, function(err) {
-      if (err) return console.log(err);
-      console.log('> Sending password reset email to:', info.email);
-    });
-  });
-};
+    Self.app.models.Email.send(
+      {
+        to: info.email,
+        from,
+        subject: 'Reset Password',
+        html
+      },
+      function (err) {
+        if (err) return console.log(err)
+        console.log('> Sending password reset email to:', info.email)
+      }
+    )
+  })
+}
